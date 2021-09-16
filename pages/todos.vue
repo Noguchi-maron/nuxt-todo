1257faf50de52a101049a5a15201c9f2b411d058
Noguchi-maron <mimikkku1radi@gmail.com>
2021-09-17 02:23:21 +0900

    動画の通りに実装

---
+++ pages/todos.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <span v-if="todo.created">
+          <input type="checkbox" :checked="todo.done" @change="toggle(todo)">
+          <span :class="{ done: todo.done }">
+          {{ todo.name }} {{ todo.created.toDate() | dateFilter }}
+          </span>
+          <button @click="remove(todo.id)">x</button>
+        </span>
+      </li>
+    </ul>
+    <div class="form">
+      <form @submit.prevent="add">
+        <input type="text" v-model="name">
+        <button>Add</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  import moment from 'moment'
+  export default {
+    data () {
+      return {
+        name: '',
+        done: false
+      }
+    },
+    created () {
+      this.$store.dispatch('todos/init')
+    },
+    methods: {
+      add () {
+        this.$store.dispatch('todos/add', this.name)
+        this.name = ''
+      },
+      remove(id) {
+        this.$store.dispatch('todos/remove', id)
+      },
+      toggle(todo) {
+        this.$store.dispatch('todos/toggle', todo)
+      }
+    },
+    computed: {
+      todos () {
+        return this.$store.getters['todos/orderdTodos']
+      }
+    },
+    filters: {
+      dateFilter (date) {
+        return moment(date).format('YYYY/MM/DD HH:mm:ss')
+      }
+    }
+  }
+</script>
+
+<style>
+li > span > span.done {
+  text-decoration: line-through;
+}
+</style>
